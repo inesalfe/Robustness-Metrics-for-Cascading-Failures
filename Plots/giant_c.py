8efa0378ed98748409fdaf0f5c4c8e355b046825
Inês Ferreira <ines.alves.ferreira@tecnico.ulisboa.pt>
2021-10-18 22:47:00 +0100

    Plots

--- Plots/giant_c.py
+++ Plots/giant_c.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open('data_0.txt') as f:
+with open('../Data/data_0.txt') as f:
     lines = f.read().splitlines()
 
 N = int(lines[0].split()[1])
@@ -17,28 +17,13 @@ giant_c_avg_rand = np.zeros(len(alphas))
 giant_c_var_rand = np.zeros(len(alphas))
 giant_c_sizes_rand = np.zeros((len(alphas), IT*N_GRAPHS))
 
-deleted_v = [[] for _ in range(len(alphas))]
-big_del = [[] for _ in range(len(alphas))]
-small_del = [[] for _ in range(len(alphas))]
-avg_del = [[] for _ in range(len(alphas))]
-
 l_it = 0
 for net in range(N_GRAPHS):
 	for a in range(len(alphas)):
 		for it in range(IT):
-			deleted_v[a].append(lines[5+5*l_it].split()[1:])
 			giant_c_sizes_rand[a][a_it[a]] = float(lines[8+5*l_it].split()[1])
 			l_it += 1
 			a_it[a] += 1
-
-for a in range(len(alphas)):
-	max_len = np.max([len(v) for v in deleted_v[a]])
-	for l in deleted_v[a]:
-		l.extend([0]*(max_len-len(l)))
-	for it in range(max_len):
-		big_del[a].append(np.max([int(v[it]) for v in deleted_v[a]]))
-		small_del[a].append(np.min([int(v[it]) for v in deleted_v[a]]))
-		avg_del[a].append(int(np.mean([int(v[it]) for v in deleted_v[a]])))
 
 giant_c_sizes_rand = giant_c_sizes_rand / N
 
@@ -46,7 +31,7 @@ for a in range(len(alphas)):
 	giant_c_avg_rand[a] = np.mean(giant_c_sizes_rand[a])
 	giant_c_var_rand[a] = np.var(giant_c_sizes_rand[a])
 
-with open('data_1.txt') as f:
+with open('../Data/data_1.txt') as f:
 	lines = f.read().splitlines()
 
 N = int(lines[0].split()[1])
@@ -60,54 +45,13 @@ giant_c_avg_bc = np.zeros(len(alphas))
 giant_c_var_bc = np.zeros(len(alphas))
 giant_c_sizes_bc = np.zeros((len(alphas), IT*N_GRAPHS))
 
-deleted_v = [[] for _ in range(len(alphas))]
-big_del = [[] for _ in range(len(alphas))]
-small_del = [[] for _ in range(len(alphas))]
-avg_del = [[] for _ in range(len(alphas))]
-
 l_it = 0
 for net in range(N_GRAPHS):
 	for a in range(len(alphas)):
 		for it in range(IT):
-			deleted_v[a].append(lines[5+5*l_it].split()[1:])
 			giant_c_sizes_bc[a][a_it[a]] = float(lines[8+5*l_it].split()[1])
 			l_it += 1
 			a_it[a] += 1
-
-for a in range(len(alphas)):
-	max_len = np.max([len(v) for v in deleted_v[a]])
-	for l in deleted_v[a]:
-		l.extend([0]*(max_len-len(l)))
-	for it in range(max_len):
-		big_del[a].append(np.max([int(v[it]) for v in deleted_v[a]]))
-		small_del[a].append(np.min([int(v[it]) for v in deleted_v[a]]))
-		avg_del[a].append(int(np.mean([int(v[it]) for v in deleted_v[a]])))
-
-f2 = plt.figure()
-
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-
-x = [range(len(avg_del[2]))]
-
-plt.plot(x, avg_del[2], color="blue")
-plt.xscale('log')
-
-plt.grid()
-# plt.legend()
-# plt.xlabel(r'\textbf{$\alpha$}', fontsize=11)
-# plt.ylabel(r'\textbf{N\'/N}', fontsize=11)
-# error = np.array([small_del[2], big_del[2]])
-# np.resize(error,(2,len(small_del[2])))
-# print(error.shape)
-# plt.errorbar(x, avg_del[2], yerr=error, fmt='bo', markersize=5, capsize=5, ecolor="black")
-
-# plt.xlim((-1.1, 11.1))
-# plt.ylim((-0.1, 1.1))
-
-plt.show()
-
-f2.savefig("g_size.png", bbox_inches='tight')
 
 giant_c_sizes_bc = giant_c_sizes_bc / N
 
@@ -115,7 +59,7 @@ for a in range(len(alphas)):
 	giant_c_avg_bc[a] = np.mean(giant_c_sizes_bc[a])
 	giant_c_var_bc[a] = np.var(giant_c_sizes_bc[a])
 
-with open('data_2.txt') as f:
+with open('../Data/data_2.txt') as f:
 	lines = f.read().splitlines()
 
 N = int(lines[0].split()[1])
@@ -143,7 +87,7 @@ for a in range(len(alphas)):
 	giant_c_avg_dg[a] = np.mean(giant_c_sizes_dg[a])
 	giant_c_var_dg[a] = np.var(giant_c_sizes_dg[a])
 
-with open('data_3.txt') as f:
+with open('../Data/data_3.txt') as f:
     lines = f.read().splitlines()
 
 N = int(lines[0].split()[1])
---
+++ Plots/node_deletion.py
@@ -0,0 +1,76 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+
+criteria = 0
+while True:
+	try:
+		criteria = int(input("Choose criteria (0, 1, 2 or 3): "))       
+	except ValueError:
+		print("Please enter an integer between 0 and 3:")
+		continue
+	else:
+		if criteria >= 0 and criteria <= 3:
+			break
+		else:
+			continue
+
+file_name = "../Data/data_%i.txt" % criteria
+
+with open(file_name) as f:
+	lines = f.read().splitlines()
+
+N = int(lines[0].split()[1])
+N_GRAPHS = int(lines[1].split()[1])
+IT = int(lines[2].split()[1])
+alphas = lines[4].split()[1:12]
+
+deleted_v = [[] for _ in range(len(alphas))]
+big_del = [[] for _ in range(len(alphas))]
+small_del = [[] for _ in range(len(alphas))]
+avg_del = [[] for _ in range(len(alphas))]
+
+l_it = 0
+for net in range(N_GRAPHS):
+	for a in range(len(alphas)):
+		for it in range(IT):
+			deleted_v[a].append(lines[5+5*l_it].split()[1:])
+			l_it += 1
+
+for a in range(len(alphas)):
+	max_len = np.max([len(v) for v in deleted_v[a]])
+	for l in deleted_v[a]:
+		l.extend([0]*(max_len-len(l)))
+	for it in range(max_len):
+		big_del[a].append(np.max([int(v[it]) for v in deleted_v[a]]))
+		small_del[a].append(np.min([int(v[it]) for v in deleted_v[a]]))
+		avg_del[a].append(int(np.mean([int(v[it]) for v in deleted_v[a]])))
+
+f1 = plt.figure()
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+x = list(range(1, len(avg_del[2])+1))
+
+plt.plot(x, avg_del[2], color="blue", label="?")
+
+er_inferior = [x1 - x2 for (x1, x2) in zip(avg_del[2], small_del[2])]
+er_superior = [x1 - x2 for (x1, x2) in zip(big_del[2], avg_del[2])]
+
+error = np.array([er_inferior, er_superior])
+np.resize(error,(2,len(small_del[2])))
+plt.errorbar(x, avg_del[2], yerr=error, fmt='bo', markersize=5, capsize=5, ecolor="black")
+
+plt.xscale('log')
+plt.grid()
+plt.legend()
+plt.xlabel(r'Time Stamp', fontsize=11)
+plt.ylabel(r'\# Deleted Nodes', fontsize=11)
+# plt.xlim((-1.1, 11.1))
+# plt.ylim((-0.1, 1.1))
+
+plt.show()
+
+f1.savefig("v_del.png", bbox_inches='tight')
