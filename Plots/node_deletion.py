9bdee5e6d8a6bb76248112c8b2dc19f1882ab1a6
Inês Ferreira <ines.alves.ferreira@tecnico.ulisboa.pt>
2021-10-19 14:01:57 +0100

    Finished Plots

--- Plots/node_deletion.py
+++ Plots/node_deletion.py
@@ -27,9 +27,11 @@ IT = int(lines[2].split()[1])
 alphas = lines[4].split()[1:12]
 
 deleted_v = [[] for _ in range(len(alphas))]
-big_del = [[] for _ in range(len(alphas))]
-small_del = [[] for _ in range(len(alphas))]
+max_del = [[] for _ in range(len(alphas))]
+min_del = [[] for _ in range(len(alphas))]
 avg_del = [[] for _ in range(len(alphas))]
+var_del = [[] for _ in range(len(alphas))]
+std_del = [[] for _ in range(len(alphas))]
 
 l_it = 0
 for net in range(N_GRAPHS):
@@ -42,10 +44,15 @@ for a in range(len(alphas)):
 	max_len = np.max([len(v) for v in deleted_v[a]])
 	for l in deleted_v[a]:
 		l.extend([0]*(max_len-len(l)))
-	for it in range(max_len):
-		big_del[a].append(np.max([int(v[it]) for v in deleted_v[a]]))
-		small_del[a].append(np.min([int(v[it]) for v in deleted_v[a]]))
-		avg_del[a].append(int(np.mean([int(v[it]) for v in deleted_v[a]])))
+	for it in range(1, max_len):
+		avg = int(np.mean([int(v[it]) for v in deleted_v[a]]))
+		if (avg == 0):
+			break
+		max_del[a].append(np.max([int(v[it]) for v in deleted_v[a]]))
+		min_del[a].append(np.min([int(v[it]) for v in deleted_v[a]]))
+		avg_del[a].append(avg)
+		var_del[a].append(np.var([int(v[it]) for v in deleted_v[a]]))
+		std_del[a] = np.sqrt(var_del[a])
 
 f1 = plt.figure()
 
@@ -54,23 +61,20 @@ plt.rc('font', family='serif')
 
 x = list(range(1, len(avg_del[2])+1))
 
-plt.plot(x, avg_del[2], color="blue", label="?")
+plt.plot(x, avg_del[2], color="blue", label="Average", marker='o')
 
-er_inferior = [x1 - x2 for (x1, x2) in zip(avg_del[2], small_del[2])]
-er_superior = [x1 - x2 for (x1, x2) in zip(big_del[2], avg_del[2])]
+plt.fill_between(x, avg_del[2]-std_del[2], avg_del[2]+std_del[2], color="red", alpha=0.1, label='Standard Deviation')
 
-error = np.array([er_inferior, er_superior])
-np.resize(error,(2,len(small_del[2])))
-plt.errorbar(x, avg_del[2], yerr=error, fmt='bo', markersize=5, capsize=5, ecolor="black")
+plt.fill_between(x, min_del[2], max_del[2], color="blue", alpha=0.1, label='Maximum and Minimum')
 
 plt.yscale('log')
 plt.grid()
 plt.legend()
 plt.xlabel(r'Time Stamp', fontsize=11)
 plt.ylabel(r'\# Deleted Nodes', fontsize=11)
-# plt.xlim((-1.1, 11.1))
-# plt.ylim((-0.1, 1.1))
 
 plt.show()
 
-f1.savefig("v_del.png", bbox_inches='tight')
+file_name = "v_del_%i.png" % criteria
+
+f1.savefig(file_name, bbox_inches='tight')
