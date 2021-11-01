4df873847ccbaa0ae717e1a8602d092c2345d9a4
Inês Ferreira <ines.alves.ferreira@tecnico.ulisboa.pt>
2021-11-01 11:39:05 +0000

    Rand

--- Simulations/test_nets.C
+++ Simulations/test_nets.C
@@ -11,21 +11,21 @@ int main() {
 
 	int N = 5000;
 
-	char f_name[50] = {0};
-	strcpy(f_name, "Data/first_line.txt");
+	char f_name1[50] = {0};
+	strcpy(f_name1, "Data/first_line_2.txt");
 
-	FILE *file = fopen(f_name, "w");
-	if (file == 0) {
-		cout << "Unable to open output file " << f_name << ". Exiting\n";
+	FILE *file1 = fopen(f_name1, "w");
+	if (file1 == 0) {
+		cout << "Unable to open output file " << f_name1 << ". Exiting\n";
 		return 10;
 	}
 
-	fprintf(file, "N");
+	fprintf(file1, "N");
 
 	for(int n = 0; n < N_GRAPHS; ++n) {
 
 		char filename[50] = {0};
-		sprintf(filename, "Simulations/Graphs/pl_%d.gml", n);
+		sprintf(filename, "Simulations/Graphs/pl2_%d.gml", n);
 
 		FILE *input_file = fopen(filename, "r");
 		if (input_file == 0) {
@@ -40,14 +40,52 @@ int main() {
 
 		N = igraph_vcount(&graph);
 
-		fprintf(file, " %d", N);
+		fprintf(file1, " %d", N);
 		
 		cout << N << endl;
 	}
 
-	fprintf(file, "\n");
+	fprintf(file1, "\n");
 
-	fclose(file);
+	fclose(file1);
+
+	char f_name2[50] = {0};
+	strcpy(f_name2, "Data/first_line_4.txt");
+
+	FILE *file2 = fopen(f_name2, "w");
+	if (file2 == 0) {
+		cout << "Unable to open output file " << f_name2 << ". Exiting\n";
+		return 10;
+	}
+
+	fprintf(file2, "N");
+
+	for(int n = 0; n < N_GRAPHS; ++n) {
+
+		char filename[50] = {0};
+		sprintf(filename, "Simulations/Graphs/pl4_%d.gml", n);
+
+		FILE *input_file = fopen(filename, "r");
+		if (input_file == 0) {
+			cout << "Unable to open input file " << filename << ". Exiting\n";
+			return 11;
+		}
+
+		igraph_t graph;
+
+		igraph_read_graph_gml(&graph, input_file);
+		fclose(input_file);
+
+		N = igraph_vcount(&graph);
+
+		fprintf(file2, " %d", N);
+		
+		cout << N << endl;
+	}
+
+	fprintf(file2, "\n");
+
+	fclose(file2);
 
 	return 0;
 }
