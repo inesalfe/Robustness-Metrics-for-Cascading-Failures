a7bdd495574fee109541cf4ff2be20d134eb79da
Inês Ferreira <ines.alves.ferreira@tecnico.ulisboa.pt>
2021-11-03 10:23:59 +0000

    Correction in plots

--- Plots/node_deletion.py
+++ Plots/node_deletion.py
@@ -6,12 +6,20 @@ import matplotlib.pyplot as plt
 model = 0
 while True:
 	try:
-		model = int(input("Choose model (0 - BA; 1 - DMS; 2 - PL2; 3 - PL4; 4 - RAND; 5 - WS): "))       
+		model = int(input(r"""Choose a model:\n
+		0 - Barabasi Albert Model w/ $<k> = 4$;\n
+		1 - DMS Minimal Model w/ $<k> = 4$;\n
+		2 - Power Law Model w/ $<k> = 2$;\n
+		3 - Power Law Model w/ $<k> = 4$;\n
+		4 - Random Graph Model w/ $<k> = 4$;\n
+		5 - Watts-Strogatz Model w/ $<k> = 4$;\n
+		6 - Power Grid Network;\n
+		7 - Internet Network):"""))       
 	except ValueError:
-		print("Please enter 0, 1, 2, 3, 4 ou 5:")
+		print("Please an integer between 0 and 7:")
 		continue
 	else:
-		if model == 0 or model == 1 or model == 2 or model == 3 or model == 4 or model == 5:
+		if model >=0 or model <=7:
 			break
 		else:
 			continue
@@ -26,8 +34,12 @@ elif model == 3:
 	folder = "Data/PL4/"
 elif model == 4:
 	folder = "Data/RAND/"
-else:
+elif model == 5:
 	folder = "Data/WS/"
+elif model == 4:
+	folder = "Data/PG/"
+else:
+	folder = "Data/INT/"
 
 file_name = "data_2.txt"
 
@@ -87,17 +99,21 @@ plt.fill_between(x, avg_del[curr_alpha]-std_del[curr_alpha], avg_del[curr_alpha]
 plt.fill_between(x, min_del[curr_alpha], max_del[curr_alpha], color="blue", alpha=0.1, label='Maximum and Minimum')
 
 if model == 0:
-	plt.title(r'Barabási Albert Model $\rightarrow \alpha=0.7$')
+	plt.title(r'Barabási Albert Model w/ $<k> = 4 \rightarrow \alpha=0.2$')
 elif model == 1:
-	plt.title(r'DMS Minimal Model $\rightarrow \alpha=0.7$')
+	plt.title(r'DMS Minimal Model w/ $<k> = 4 \rightarrow \alpha=0.2$')
 elif model == 2:
-	plt.title(r'Power Law Model w/ $<k> = 2 \rightarrow \alpha=0.7$')
+	plt.title(r'Power Law Model w/ $<k> = 2 \rightarrow \alpha=0.2$')
 elif model == 3:
-	plt.title(r'Power Law Model w/ $<k> = 4 \rightarrow \alpha=0.7$')
+	plt.title(r'Power Law Model w/ $<k> = 4 \rightarrow \alpha=0.2$')
 elif model == 4:
-	plt.title(r'Random Graph Model w/ $<k> = 4 \rightarrow \alpha=0.7$')
+	plt.title(r'Random Graph Model w/ $<k> = 4 \rightarrow \alpha=0.2$')
+elif model == 5:
+	plt.title(r'Watts-Strogatz Model w/ $<k> = 4 \rightarrow \alpha=0.2$')
+elif model == 6:
+	plt.title(r'Power Grid Network $\rightarrow \alpha=0.2$')
 else:
-	plt.title(r'Watts-Strogatz Model $\rightarrow \alpha=0.7$')
+	plt.title(r'Internet Network $\rightarrow \alpha=0.2$')
 
 plt.yscale('log')
 plt.grid()
@@ -127,17 +143,21 @@ plt.fill_between(x, avg_del[curr_alpha]-std_del[curr_alpha], avg_del[curr_alpha]
 plt.fill_between(x, min_del[curr_alpha], max_del[curr_alpha], color="blue", alpha=0.1, label='Maximum and Minimum')
 
 if model == 0:
-	plt.title(r'Barabási Albert Model $\rightarrow \alpha=0.7$')
+	plt.title(r'Barabási Albert Model w/ $<k> = 4 \rightarrow \alpha=0.5$')
 elif model == 1:
-	plt.title(r'DMS Minimal Model $\rightarrow \alpha=0.7$')
+	plt.title(r'DMS Minimal Model w/ $<k> = 4 \rightarrow \alpha=0.5$')
 elif model == 2:
-	plt.title(r'Power Law Model w/ $<k> = 2 \rightarrow \alpha=0.7$')
+	plt.title(r'Power Law Model w/ $<k> = 2 \rightarrow \alpha=0.5$')
 elif model == 3:
-	plt.title(r'Power Law Model w/ $<k> = 4 \rightarrow \alpha=0.7$')
+	plt.title(r'Power Law Model w/ $<k> = 4 \rightarrow \alpha=0.5$')
 elif model == 4:
-	plt.title(r'Random Graph Model w/ $<k> = 4 \rightarrow \alpha=0.7$')
+	plt.title(r'Random Graph Model w/ $<k> = 4 \rightarrow \alpha=0.5$')
+elif model == 5:
+	plt.title(r'Watts-Strogatz Model w/ $<k> = 4 \rightarrow \alpha=0.5$')
+elif model == 6:
+	plt.title(r'Power Grid Network $\rightarrow \alpha=0.5$')
 else:
-	plt.title(r'Watts-Strogatz Model $\rightarrow \alpha=0.7$')
+	plt.title(r'Internet Network $\rightarrow \alpha=0.5$')
 
 plt.yscale('log')
 plt.grid()
@@ -167,17 +187,21 @@ plt.fill_between(x, avg_del[curr_alpha]-std_del[curr_alpha], avg_del[curr_alpha]
 plt.fill_between(x, min_del[curr_alpha], max_del[curr_alpha], color="blue", alpha=0.1, label='Maximum and Minimum')
 
 if model == 0:
-	plt.title(r'Barabási Albert Model $\rightarrow \alpha=0.7$')
+	plt.title(r'Barabási Albert Model w/ $<k> = 4 \rightarrow \alpha=0.7$')
 elif model == 1:
-	plt.title(r'DMS Minimal Model $\rightarrow \alpha=0.7$')
+	plt.title(r'DMS Minimal Model w/ $<k> = 4 \rightarrow \alpha=0.7$')
 elif model == 2:
 	plt.title(r'Power Law Model w/ $<k> = 2 \rightarrow \alpha=0.7$')
 elif model == 3:
 	plt.title(r'Power Law Model w/ $<k> = 4 \rightarrow \alpha=0.7$')
 elif model == 4:
 	plt.title(r'Random Graph Model w/ $<k> = 4 \rightarrow \alpha=0.7$')
+elif model == 5:
+	plt.title(r'Watts-Strogatz Model w/ $<k> = 4 \rightarrow \alpha=0.7$')
+elif model == 6:
+	plt.title(r'Power Grid Network $\rightarrow \alpha=0.7$')
 else:
-	plt.title(r'Watts-Strogatz Model $\rightarrow \alpha=0.7$')
+	plt.title(r'Internet Network $\rightarrow \alpha=0.7$')
 
 plt.yscale('log')
 plt.grid()
