e3a7fa5c35710b010a348e5466f65ff267121da8
Inês Ferreira <ines.alves.ferreira@tecnico.ulisboa.pt>
2021-11-03 16:40:13 +0000

    Internet finished and changes in animation

--- Animation/anim_nolabels.py
+++ Animation/anim_nolabels.py
@@ -32,14 +32,19 @@ def grouped(iterable, n):
 	return zip(*[iter(iterable)]*n)
 
 if model == 0:
-	G = nx.read_gml("Animation/Graphs/ba.gml", None)
+	G = nx.read_gml("Animation/Graphs/ba2.gml", None)
 	file_name = "Animation/Data/ba_%i.txt" % criteria
 elif model == 1:
-	G = nx.read_gml("Animation/Graphs/dms.gml", None)
+	G = nx.read_gml("Animation/Graphs/dms2.gml", None)
 	file_name = "Animation/Data/dms_%i.txt" % criteria
 else:
-	G = nx.read_gml("Animation/Graphs/small.gml", None)
+	G = nx.read_gml("Animation/Graphs/small2.gml", None)
 	file_name = "Animation/Data/small_%i.txt" % criteria
+
+for node in G.nodes():
+	G.nodes[node]['pos'] = G.nodes[node]['graphics']['x'], G.nodes[node]['graphics']['y']
+
+pos = nx.get_node_attributes(G,'pos')
 
 it_list_v = []
 it_list_e = []
@@ -59,15 +64,8 @@ with open(file_name, 'r') as f:
 			for word in words:
 				it_list_e[it].append(int(word))
 
-# print(it+1)
-# print(it_list_v)
-# print(it_list_e)
-
 N = G.number_of_nodes()
 E = G.number_of_edges()
-
-# print(N)
-# print(E)
 
 untouched_edges = list(G.edges)
 deleted_edges = []
@@ -85,12 +83,39 @@ else:
 
 fig_it = 1
 
-fig = plt.figure()
-nx.draw_networkx(G, pos=nx.kamada_kawai_layout(G), nodelist = untouched_vertices, node_color = 'blue', edgelist = untouched_edges, edge_color = 'black', alpha = 1.0, with_labels=True, font_weight='bold')
+fig = plt.figure(figsize=(15, 7))
+
+ax=plt.gca()
+
+bc = nx.betweenness_centrality(G)
+for i in range(len(bc)):
+	if (bc[i] == 0):
+		bc[i] = 0.02
+
+sizes = dict(bc)
+
+nx.draw_networkx(G, pos=pos, nodelist = untouched_vertices, node_color = 'blue', edgelist = untouched_edges, edge_color = 'black', alpha = 1.0, with_labels=False, font_weight='bold', node_size=[v * 1000 for v in sizes.values()])
+
+ax.autoscale()
+plt.axis('equal')
+plt.axis('off')
+
+plt.margins(0.0)
+
+cut = 1.1
+xmax= cut*max(xx for xx,yy in pos.values())
+ymax= cut*max(yy for xx,yy in pos.values())
+xmin= cut*min(xx for xx,yy in pos.values())
+ymin= cut*min(yy for xx,yy in pos.values())
+plt.xlim(xmin,xmax)
+plt.ylim(ymin,ymax)
+
 plt.show()
 fig_name = name + "_%i.png" % fig_it
 fig.savefig(fig_name + '', format='png')
 fig_it = fig_it + 1
+
+exit(0)
 
 for i in range(it+1):
 
@@ -104,9 +129,9 @@ for i in range(it+1):
 			marked_edges.append((e1, e2))
 
 	fig = plt.figure()
-	nx.draw_networkx(G, pos=nx.kamada_kawai_layout(G), nodelist = untouched_vertices, node_color = 'blue', edgelist = untouched_edges, edge_color = 'black', alpha = 1.0, with_labels=True, font_weight='bold')
-	nx.draw_networkx(G, pos=nx.kamada_kawai_layout(G), nodelist = marked_vertices, node_color = 'red', edgelist = marked_edges, edge_color = 'red', alpha = 1.0, with_labels=True, font_weight='bold')
-	nx.draw_networkx(G, pos=nx.kamada_kawai_layout(G), nodelist = deleted_vertices, node_color = 'grey', edgelist = deleted_edges, edge_color = 'grey', alpha = 0.2, with_labels=True, font_weight='bold')
+	nx.draw_networkx(G, pos=pos, nodelist = untouched_vertices, node_color = 'blue', edgelist = untouched_edges, edge_color = 'black', alpha = 1.0, with_labels=False, font_weight='bold')
+	nx.draw_networkx(G, pos=pos, nodelist = marked_vertices, node_color = 'red', edgelist = marked_edges, edge_color = 'red', alpha = 1.0, with_labels=False, font_weight='bold')
+	nx.draw_networkx(G, pos=pos, nodelist = deleted_vertices, node_color = 'grey', edgelist = deleted_edges, edge_color = 'grey', alpha = 0.2, with_labels=False, font_weight='bold')
 	plt.show()
 	fig_name = name + "_%i.png" % fig_it
 	fig.savefig(fig_name, format='png')
@@ -123,8 +148,8 @@ for i in range(it+1):
 	marked_edges.clear()
 
 	fig = plt.figure()
-	nx.draw_networkx(G, pos=nx.kamada_kawai_layout(G), nodelist = untouched_vertices, node_color = 'blue', edgelist = untouched_edges, edge_color = 'black', alpha = 1.0, with_labels=True, font_weight='bold')
-	nx.draw_networkx(G, pos=nx.kamada_kawai_layout(G), nodelist = deleted_vertices, node_color = 'grey', edgelist = deleted_edges, edge_color = 'grey', alpha = 0.2, with_labels=True, font_weight='bold')
+	nx.draw_networkx(G, pos=pos, nodelist = untouched_vertices, node_color = 'blue', edgelist = untouched_edges, edge_color = 'black', alpha = 1.0, with_labels=False, font_weight='bold')
+	nx.draw_networkx(G, pos=pos, nodelist = deleted_vertices, node_color = 'grey', edgelist = deleted_edges, edge_color = 'grey', alpha = 0.2, with_labels=False, font_weight='bold')
 	plt.show()
 	fig_name = name + "_%i.png" % fig_it
 	fig.savefig(fig_name, format='png')
